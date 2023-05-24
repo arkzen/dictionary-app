ca6e485c7d6d5b552d873ff7ccac37cc816ad3c0
Asikur <asikurr817@gmail.com>
2023-05-24 19:13:12 +0600

    Repository interface updated

--- app/src/main/java/studios/darkzen/dictionaryapp/service/repository/DictionaryRepository.java
+++ app/src/main/java/studios/darkzen/dictionaryapp/service/repository/DictionaryRepository.java
@@ -2,9 +2,10 @@ package studios.darkzen.dictionaryapp.service.repository;
 
 import androidx.lifecycle.MutableLiveData;
 
-import studios.darkzen.dictionaryapp.service.model.ApiResponseData;
+import studios.darkzen.dictionaryapp.service.model.RootResponse;
 
 public interface DictionaryRepository {
 
-   MutableLiveData<ApiResponseData> getApiResponse();
+   MutableLiveData<RootResponse> getApiResponse(String word);
+
 }
